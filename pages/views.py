d729a5f04f1e9e2d63685222fe59e51b0a319801
Mike M <mikemaddem@gmail.com>
2017-12-08 15:05:58 -0500

    mike fixed it, hahaha bitch

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views import generic
+from django.views.generic import View
 # Create your views here.
 
 def index(request):
-    template_name = 'pages/index.html;'
+    template_name = 'pages/index.html'
     return render(request, template_name)
     #return HttpResponse("TEST")
