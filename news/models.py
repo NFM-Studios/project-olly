c4e52acdc87e3eb513aa8dd38c44f53a4d6c8961
Mike Madden <mikemaddem@gmail.com>
2018-01-11 23:13:14 +0000

    News

---
+++ news/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+
+# not working
+# from django.core.urlresolvers import reverse
+
+from django.urls import reverse
+
+#tag manager
+from taggit.managers import TaggableManager
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+class Post(models.Model): 
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('scheduled', 'Scheduled'),
+        ('published', 'Published'),
+        ('private', 'Private'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author= models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+    body= models.TextField()
+    publish= models.DateTimeField(default=timezone.now)
+    created= models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
+    status= models.CharField(max_length=12, choices=STATUS_CHOICES, default='draft')
+    #scheduled= (date time)
+
+    #default manager
+    objects = models.Manager()
+    
+    #specific manager
+    published = PublishedManager()
+
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ('-publish',)
+    
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('news:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created= models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
+
+
