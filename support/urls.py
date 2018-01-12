37666493c0bb3c3ef9e1f37fee0151824ca1b8d8
Mike M <mikemaddem@gmail.com>
2018-01-12 16:53:39 -0500

    Nothing

--- support/urls.py
+++ support/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-
 from support.views import MyTicketListView, MyTicketDetailView, TicketCreateView
 
 app_name = 'support'
