b105cf5035a3799af239846a0faa09fac5bbf9e4
Steven Young <steven97@me.com>
2018-01-17 16:07:54 +0000

    Merge Bans

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,5 +7,5 @@ app_name='profiles'
 urlpatterns = [
     url(r'^$', views.profile_no_username, name='profile_no_username'),
     url(r'^edit/$', views.edit_profile, name='edit_profile'),
-    url(r'^user/(?P<urlusername>\w+)/$', views.profile, name='profile'),
+    url(r'^user/(?P<urlusername>\w+)/$', views.profile, name='profile')
 ]
