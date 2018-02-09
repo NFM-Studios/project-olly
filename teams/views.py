8db7373fd21154cd12bc5a635f69ad167f9c3ca8
Mike M <mikemaddem@gmail.com>
2018-02-09 15:55:21 -0500

    logic for the team invite form

--- teams/views.py
+++ teams/views.py
@@ -28,12 +28,30 @@ class MyTeamsListView(ListView):
 # list all the teams they are apart of
 # maybe list the role they have?
 
+    def get_queryset(self):
+        return Team.objects.filter(players=self.request.user)
+
 
 class MyTeamDetailView(DetailView):
 #show team info, allow them to invite users.
+    model = Team
+    #base team template all users can see, inside the template some permissions like viewing the edit team button
+    # will be managed
+    template_name = 'teams/team.html'
 
 class TeamInviteCreateView(CreateView):
 #allow the person to create an invite for there team
+    form_class = TeamInviteCreateForm
+    template_name = 'teams/invite-player.html'
+
+    def form_valid(self, form):
+        TeamInvite = form.instance
+        TeamInvite.creator = self.request.user
+        TeamInvite.save()
+        self.success_url = reverse('teams:detail', args=[team.id])
+        messages.success(self.request, 'Your invite has been successfully sent')
+        return super(TeamInviteCreateView, self).form_valid(form)
+
 
 class CaptainInviteCreateView(CreateView):
     form_class = CaptainInviteForm
