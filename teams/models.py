9546d6e8c03cb1f89784e83b227f3412979928b5
Mike M <mikemaddem@gmail.com>
2018-02-09 15:12:19 -0500

    added brief comments to help  separate each field

--- teams/models.py
+++ teams/models.py
@@ -7,16 +7,27 @@ from profiles import UserProfile
 from django.urls import reverse
 
 class Team(models.Model):
+    #team name
     name = models.CharField(max_length=25)
+    #team bio
     about_us = models.CharField(max_length=250, default='Forever a mystery', blank=True)
+    #not sure if we really need the earnings, but its here for now...
     total_earning = models.PositiveSmallIntegerField(default=0)
+    # link your website in case its an org?
     website = models.CharField(max_length=100, default='No Website', blank=True)
+    #link ur twitter, and maybe eventually embed the twitter feed
     twitter = models.CharField(max_length=15, default='No Twitter Linked', blank=True)
+    #link ur twitch, maybe integrate the twitch api
     twitch = models.CharField(max_length=15, default='No Twitch Linked', blank=True)
+    #whoever filled out the form to create the team, limited to only one
     founder = models.ForeignKey(User, related_name='founder', on_delete=models.CASCADE)
+    #basically founder permissions, but to other people that didnt create the actual team
     captain = models.ForeignKey(User, related_name='captain', on_delete=models.CASCADE)
+    #the people of the actual team, now a many to many, not a forkey
     players = models.ManyToManyField(Player, through='TeamInvite', through_fields=('team', 'user'))
+    #when they created the team
     created= models.DateTimeField(auto_now_add=True)
+    #when they last updated anything in the team
     updated= models.DateTimeField(auto_now=True)
 
 class TeamInvite(models.Model):
