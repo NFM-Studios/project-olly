87e95c1f312cc7e3fc50698e57da582e2a4f9155
Mike M <mikemaddem@gmail.com>
2018-02-09 15:34:53 -0500

    spelling error

--- teams/forms.py
+++ teams/forms.py
@@ -6,7 +6,7 @@ from teams.models import Team
 #import the model for the team invite
 from teams.models import TeamInvite
 
-fron teams.models import CaptainInvite
+from teams.models import CaptainInvite
 
 #forms to create a team of various sizes
 
@@ -14,9 +14,6 @@ class TeamCreateForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = ('name')
-
-
-
 
 #invite forms to invite players to a team
 
