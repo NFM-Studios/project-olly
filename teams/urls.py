3cbafc6d594af22d07596773615610a371d7b8e8
Mike M <mikemaddem@gmail.com>
2018-02-14 16:25:01 -0500

    fixed runtime errors with form names

--- teams/urls.py
+++ teams/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from teams.views import MyTeamListView, MyTeamDetailView, TeamCreateView
+from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView
 
 app_name = 'teams'
 
 urlpatterns = [
-    url(r'^my/$', login_required(MyTeamListView.as_view()), name='list'),
+    url(r'^my/$', login_required(MyTeamsListView.as_view()), name='list'),
     url(r'^/(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
     url(r'^create/$', login_required(TeamCreateView.as_view()), name='create'),
 ]
