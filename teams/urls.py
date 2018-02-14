cbeb5ea60928da1d0b9979171e03a88701b58286
Mike M <mikemaddem@gmail.com>
2018-02-14 15:57:44 -0500

    URLs initial commit

---
+++ teams/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from teams.views import MyTeamListView, MyTeamDetailView, TeamCreateView
+
+app_name = 'support'
+
+urlpatterns = [
+    url(r'^my/$', login_required(MyTeamListView.as_view()), name='list'),
+    url(r'^/(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
+    url(r'^create/$', login_required(TeamCreateView.as_view()), name='create'),
+]
