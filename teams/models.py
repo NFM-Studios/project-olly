a6b42517ba04393dbbb796f9ee4b730e753843cc
Mike M <mikemaddem@gmail.com>
2018-02-14 15:51:49 -0500

    fixed related names, and finished captain mtm field

--- teams/models.py
+++ teams/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import IntegrityError
-from profiles import UserProfile
+#from profiles import UserProfile
 
 from django.urls import reverse
 
@@ -22,9 +22,9 @@ class Team(models.Model):
     #whoever filled out the form to create the team, limited to only one
     founder = models.ForeignKey(User, related_name='founder', on_delete=models.CASCADE)
     #basically founder permissions, but to other people that didnt create the actual team
-    captain = models.ManyToManyField(User, through='')
+    captain = models.ManyToManyField(User, through='CaptainInvite', through_fields=('team','user','inviter'), related_name='teamcaptain')
     #the people of the actual team, now a many to many, not a forkey
-    players = models.ManyToManyField(User, through='TeamInvite', through_fields=('team', 'user'))
+    players = models.ManyToManyField(User, through='TeamInvite', through_fields=('team','user','inviter'))
     #when they created the team
     created= models.DateTimeField(auto_now_add=True)
     #when they last updated anything in the team
@@ -40,18 +40,18 @@ class Team(models.Model):
 
 class TeamInvite(models.Model):
     expire = models.DateTimeField(auto_now=False, auto_now_add=False)
-    team = models.ForeignKey(Team, related_name='invited-to', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='to-invite', on_delete=models.CASCADE)
-    inviter = models.ForeignKey(User, related_name='from-invite', on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, related_name='invitedto', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='toinvite', on_delete=models.CASCADE)
+    inviter = models.ForeignKey(User, related_name='frominvite', on_delete=models.CASCADE)
     accepted = models.BooleanField(default=False)
     declined = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
 
 class CaptainInvite(models.Model):
     expire = models.DateTimeField(auto_now=False, auto_now_add=False)
-    team = models.ForeignKey(Team, related_name='invited-to', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='to-captain', on_delete=models.CASCADE)
-    inviter = models.ForeignKey(User, related_name='from-captain', on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, related_name='captainto', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='tocaptain', on_delete=models.CASCADE)
+    inviter = models.ForeignKey(User, related_name='fromcaptain', on_delete=models.CASCADE)
     accepted = models.BooleanField(default=False)
     declined = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
