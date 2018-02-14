ca0e672a752bef494265ba8e9a752ef3575e3195
Mike M <mikemaddem@gmail.com>
2018-02-14 16:19:29 -0500

    specify app name

--- teams/urls.py
+++ teams/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from teams.views import MyTeamListView, MyTeamDetailView, TeamCreateView
 
-app_name = 'support'
+app_name = 'teams'
 
 urlpatterns = [
     url(r'^my/$', login_required(MyTeamListView.as_view()), name='list'),
