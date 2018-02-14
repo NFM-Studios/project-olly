6bb911ff30ae31f967b932b8846c02f9f3fc014c
Mike M <mikemaddem@gmail.com>
2018-02-14 16:29:33 -0500

    removed leading slash, added invite and import view

--- teams/urls.py
+++ teams/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView
+from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView, TeamInviteCreateView
 
 app_name = 'teams'
 
 urlpatterns = [
     url(r'^my/$', login_required(MyTeamsListView.as_view()), name='list'),
-    url(r'^/(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
+    url(r'^(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
     url(r'^create/$', login_required(TeamCreateView.as_view()), name='create'),
+    url(r'^invite/$', login_required(TeamInviteCreateView.as_view()), name='invite'),
 ]
