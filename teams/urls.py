c257bebfb4ce3a1ebfd2b33e2687c604c5fd467e
Mike M <mikemaddem@gmail.com>
2018-02-15 15:51:18 -0500

    add captain invite route, import captain view.

--- teams/urls.py
+++ teams/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView, TeamInviteCreateView
+from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView, TeamInviteCreateView, CaptainInviteCreateView
 
 app_name = 'teams'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
     url(r'^create/$', login_required(TeamCreateView.as_view()), name='create'),
     url(r'^invite/$', login_required(TeamInviteCreateView.as_view()), name='invite'),
+    url(r'^captain/$', login_required(CaptainInviteCreateView.as_view()), name='captaininvite'),
+
 ]
