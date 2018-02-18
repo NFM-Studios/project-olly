1980d63b5c755d77893772c8e4f362970e721bd4
Mike M <mikemaddem@gmail.com>
2018-02-18 15:41:59 -0500

    Removed captain invite urls

--- teams/urls.py
+++ teams/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.decorators import login_required
-from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView, TeamInviteCreateView, CaptainInviteCreateView
+from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView, TeamInviteCreateView
 
 app_name = 'teams'
 
@@ -9,8 +9,8 @@ urlpatterns = [
     url(r'^my/$', login_required(MyTeamsListView.as_view()), name='list'),
     url(r'^(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
     url(r'^create/$', login_required(TeamCreateView.as_view()), name='create'),
-    url(r'^invite/$', login_required(TeamInviteCreateView.as_view()), name='invite'),
-    url(r'^captain/$', login_required(CaptainInviteCreateView.as_view()), name='captaininvite'),
+    #url(r'^invite/$', login_required(TeamInviteCreateView.as_view()), name='invite'),
+    #url(r'^captain/$', login_required(CaptainInviteCreateView.as_view()), name='captaininvite'),
     url(r'^edit/(?P<pk>\d+)/$', login_required(views.EditTeamView), name='edit'),
 
 
