487cb3e9444646ed2fb999635966dc76df439b3e
Mike M <mikemaddem@gmail.com>
2018-02-21 14:49:08 -0500

    remove captain invite url

--- teams/urls.py
+++ teams/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^my/$', login_required(MyTeamsListView.as_view()), name='list'),
     url(r'^(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
     url(r'^create/$', login_required(TeamCreateView.as_view()), name='create'),
-    #url(r'^invite/$', login_required(TeamInviteCreateView.as_view()), name='invite'),
+    url(r'^invite/$', login_required(TeamInviteCreateView.as_view()), name='invite'),
     #url(r'^captain/$', login_required(CaptainInviteCreateView.as_view()), name='captaininvite'),
     url(r'^edit/(?P<pk>\d+)/$', login_required(views.EditTeamView), name='edit'),
 
