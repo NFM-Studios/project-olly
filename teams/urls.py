fede8219a6289f52f8dc81747f7f54ec1f027d61
Mike M <mikemaddem@gmail.com>
2018-02-21 14:54:51 -0500

    import my invite list view. remove captain invite. add invites list view

--- teams/urls.py
+++ teams/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.decorators import login_required
-from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView, TeamInviteCreateView
+from teams.views import MyTeamsListView, MyTeamDetailView, TeamCreateView, TeamInviteCreateView, MyInvitesListView
 
 app_name = 'teams'
 
 urlpatterns = [
     url(r'^my/$', login_required(MyTeamsListView.as_view()), name='list'),
-    url(r'^(?P<pk>\d+)/$', login_required(MyTeamDetailView.as_view()), name='detail'),
+    url(r'^invites/$', login_required(MyInvitesListView.as_view()), name='myinvitelist'),
+    url(r'^(?P<pk>\d+)/$', MyTeamDetailView.as_view(), name='detail'),
     url(r'^create/$', login_required(TeamCreateView.as_view()), name='create'),
     url(r'^invite/$', login_required(TeamInviteCreateView.as_view()), name='invite'),
-    #url(r'^captain/$', login_required(CaptainInviteCreateView.as_view()), name='captaininvite'),
     url(r'^edit/(?P<pk>\d+)/$', login_required(views.EditTeamView), name='edit'),
 
 
