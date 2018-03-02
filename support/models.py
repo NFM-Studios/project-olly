da4aad691a783140b98ae3497a3aaab488796167
Mike M <mikemaddem@gmail.com>
2018-03-01 19:28:16 -0500

    Added category field to support tickets

--- support/models.py
+++ support/models.py
@@ -5,13 +5,23 @@ from support.settings import STATUS_CHOICES, CLOSED_STATUSES
 
 # Create your models here.
 
+CATEGORY_CHOICES = (
+    ('general', 'General'),
+    ('prize claim', 'Prize Claim'),
+    ('tournament support', 'Tournament Support'),
+    ('billing', 'Billing'),
+    ('refund request', 'Refund Request'),
+    ('psn/xbl issues', 'PSN/XBL Issues'),
+    ('match support/dispute','Match Support/Dispute'),
+)
 
 class Ticket(models.Model):
     creator = models.ForeignKey(User, verbose_name='Creator', related_name='ticket_create', on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=False, auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True, auto_now_add=False)
     subject = models.CharField(max_length=255)
-    text = models.TextField(default='idiot didnt enter any text', help_text='A detailed description of your issue')
+    category = models.CharField(max_length=15, choices=CATEGORY_CHOICES, default='general')
+    text = models.TextField(default='A detailed description of your issue')
     assignee = models.ForeignKey(User, related_name='assigned_tickets', verbose_name='assignee', blank=True,
                                  null=True, on_delete=models.CASCADE)
     status = models.SmallIntegerField(choices=STATUS_CHOICES, default=0)
@@ -48,7 +58,7 @@ class TicketComment(models.Model):
     date = models.DateTimeField(auto_now_add=True, verbose_name='Date')
     author = models.ForeignKey(User, verbose_name='Author', on_delete=models.CASCADE)
     comment = models.TextField()
-    
+
     class Meta:
         verbose_name = 'Ticket comment'
         verbose_name_plural = 'Ticket comments'
