8739b14571b2fb8139e338efbfad68556d0c1e9a
Mike M <mikemaddem@gmail.com>
2018-03-17 14:56:09 -0400

    added prizes and a lot of comments

--- singletournaments/models.py
+++ singletournaments/models.py
@@ -3,6 +3,14 @@ from matches.settings import GAME_CHOICES, PLATFORMS_CHOICES, TEAMFORMAT_CHOICES
 from matches.models import RuleSet
 from teams.models import Team
 
+SIZE_CHOICES= (
+    (4,4),
+    (8,8),
+    (16,16),
+    (32,32),
+    (64,64),
+    (128,128),
+)
 
 class SingleEliminationTournament(models.Model):
 
@@ -38,9 +46,76 @@ class SingleEliminationTournament(models.Model):
     # specify second place, just for storage and future reference
     second = models.ForeignKey(Team, related_name='secondplaceteam', on_delete=models.CASCADE)
     # specify how many teams the event will be capped at, and the size of the bracket
-    size = models.PositiveSmallIntegerField(default=32)
+    size = models.PositiveSmallIntegerField(default=32, choices=SIZE_CHOICES)
+
+    prize1 = models.CharField(default='no prize specified', related_name='firstplaceprize')
+    prize2 = models.CharField(default='no prize specified', related_name='secondplaceprize')
+    prize3 = models.CharField(default='no prize specified', related_name='thirdplaceprize')
 
     # need to figure out how we will work rules
     rules = models.ForeignKey(RuleSet, related_name='tournamentrules', on_delete=models.CASCADE)
 
+    def generate_bracket(self):
+        # size = size
+        # bye = size
+        if size == 4:
+            # 1 play 4
+            # 2 plays 3
+            # 2 matches need to be played in round 1
+            # 1 match needs to be played in round 2
+            # total number of rounds = 2
+        elif size == 8:
+            # 1 plays 8
+            # 2 plays 7
+            # 3 plays 6
+            # 4 plays 5
+            # 4 matches need to be played in round 1
+            # total number of rounds = 3
+        elif size == 16:
+            # 1 plays 16
+            # 2 plays 15
+            # 3 plays 14
+            # 4 plays 13
+            # 5 plays 12
+            # and so on
+            # 8 matches need to be played in round 1
+            # total number of rounds = 4
+        elif size == 32:
+            # 1 plays 32
+            # 2 plays 31
+            # and so on
+            # 16 matches need to be played in round 1
+            # 8 round 2
+            # 4 round 3
+            # 2 round 4
+            # 1 round 5 (winners match)
+            # total number of rounds = 5
 
+        elif size == 64:
+            # the same thing
+            # 32 matches need to be played in round 1
+            # 16 round 2
+            # 8 round 3
+            # 4 round 4
+            # 2 round 5
+            # 1 round 6 (winners match)
+            # total number of rounds = 6
+
+        elif size == 128:
+            # same thing
+            # 64 matches need to be played in round 1
+            # 32 round 2
+            # 16 round 3
+            # 8 round 4
+            # 4 round 5
+            # 2 round 6
+            # 1 round 7
+            # total number of rounds = 7
+
+
+    def generate_matches(self):
+        # a
+
+    def rounds_required(self):
+        # the number of
+        # teams/2
