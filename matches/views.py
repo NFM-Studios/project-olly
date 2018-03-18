1ac65ef1910841a0f858a79f0cf06b11d390fb10
Mike M <mikemaddem@gmail.com>
2018-03-18 14:09:16 -0400

    import proper packages and start detail view for matches

--- matches/views.py
+++ matches/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from singletournaments.models import SingleEliminationTournament
+from teams.models import Team
+from matches.models import Match
 
-# Create your views here.
+
+class TournamentMatchDetailView(DetailView):
+    model = Match
+
