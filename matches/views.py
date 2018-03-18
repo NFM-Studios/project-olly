3732967ccbce031538c6743af48bfbaa9763130f
Mike M <mikemaddem@gmail.com>
2018-03-18 18:18:41 -0400

    match report create view progress

--- matches/views.py
+++ matches/views.py
@@ -8,3 +8,15 @@ from matches.models import Match
 class TournamentMatchDetailView(DetailView):
     model = Match
 
+class MatchReportCreateView(CreateView):
+    form_class = MatchReportCreateForm
+    template_name = 'matches/matches_report.html'
+
+    def form_valid(self, form):
+        report = form.instance
+        report.reporter = self.request.user
+        report.
+        report.save()
+        self.success_url = reverse('matches:detail', args=[matches.id])
+        messages.success(self.request, 'Your Report has been successfully submitted')
+        return super(MatchReportCreateView, self).form_valid(form)
