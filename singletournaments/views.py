80c948832165859ca04c2347b1f9b57d213cc1c6
Mike M <mikemaddem@gmail.com>
2018-03-25 14:15:52 -0400

    get function get proper rounds based on size and tournament

--- singletournaments/views.py
+++ singletournaments/views.py
@@ -36,3 +36,22 @@ class SingleTournamentRules(View):
 
 class SingleTournamentBracket(View):
     template_name = 'singletournaments/singletournament_bracket.html'
+
+    def get(self, request, **kwargs):
+        pk = self.kwargs['pk']
+        tournament = SingleEliminationTournament.objects.get(id=pk)
+        if  tournament.size == 4:
+            # get 2 rounds to pass to the view
+            round1 = SingleTournamentRound.get(tournament=tournament, roundnum=1)
+            round2 = SingleTournamentRound.get(tournament=tournament, roundnum=2)
+        elif tournament.size == 8:
+            # get 3 rounds to pass to the view
+        elif tournament.size == 16:
+            # get 4 rounds to pass to the view
+        elif tournament.size == 32:
+            # get 5 rounds to pass to the view
+        elif tournamenet.size == 64:
+            # get 6 rounds to pass to the view
+        elif tournament.size == 128:
+            # get 7 rounds to pass to the  view
+        return render(request, self.template_name, {'x': pk, 'tournament': tournament, 'round1':round1, 'round2':round2})
