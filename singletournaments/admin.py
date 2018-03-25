cc392d49608af34a02fa43a41ddd3fab87d90f36
Mike M <mikemaddem@gmail.com>
2018-03-25 14:52:50 -0400

    fix for testing

--- singletournaments/admin.py
+++ singletournaments/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import SingleTournamentTeam, SingleEliminationTournament, SingleTournamentRound
 
-admin.site.register(SingleEliminationTournament, SingleTournamentRound, SingleTournamentTeam)
+admin.site.register(SingleEliminationTournament)
