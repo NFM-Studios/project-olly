4e2faf81566164e6037f1c7bb5ba4cf0604be9f2
Mike M <mikemaddem@gmail.com>
2018-03-25 14:27:13 -0400

    determine the template in the view

--- singletournaments/views.py
+++ singletournaments/views.py
@@ -44,14 +44,20 @@ class SingleTournamentBracket(View):
             # get 2 rounds to pass to the view
             round1 = SingleTournamentRound.get(tournament=tournament, roundnum=1)
             round2 = SingleTournamentRound.get(tournament=tournament, roundnum=2)
+            template_name = 'singletournaments/singletournament_bracket4.html'
         elif tournament.size == 8:
             # get 3 rounds to pass to the view
+            template_name = 'singletournaments/singletournament_bracket8.html'
         elif tournament.size == 16:
             # get 4 rounds to pass to the view
+            template_name = 'singletournaments/singletournament_bracket16.html'
         elif tournament.size == 32:
             # get 5 rounds to pass to the view
+            template_name = 'singletournaments/singletournament_bracket32.html'
         elif tournamenet.size == 64:
             # get 6 rounds to pass to the view
+            template_name = 'singletournaments/singletournament_bracket64.html'
         elif tournament.size == 128:
             # get 7 rounds to pass to the  view
+            template_name = 'singletournaments/singletournament_bracket128.html'
         return render(request, self.template_name, {'x': pk, 'tournament': tournament, 'round1':round1, 'round2':round2})
