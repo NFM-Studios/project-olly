30d6ba02fbc81221bab4b756c3dec747aa59f643
Mike M <mikemaddem@gmail.com>
2018-03-25 14:48:36 -0400

    try the admin panel for testing

--- singletournaments/admin.py
+++ singletournaments/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import SingleTournamentTeam, SingleEliminationTournament, SingleTournamentRound
 
-# Register your models here.
+admin.site.register(SingleEliminationTournament, SingleTournamentRound, SingleTournamentTeam)
