92bc1ed7fb78ec77feac3b0a832b8f72b30708b3
Mike M <mikemaddem@gmail.com>
2018-03-26 16:37:43 -0400

    fix match report url

--- matches/urls.py
+++ matches/urls.py
@@ -7,6 +7,6 @@ app_name = 'matches'
 # profile/
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', login_required(MatchDetailView.as_view()), name='detail'),
-    url(r'^(?P<pk>\d+)/report$', login_required(MyTicketDetailView.as_view()), name='detail'),
+    url(r'^(?P<pk>\d+)/report$', login_required(MatchReportCreateView.as_view()), name='report'),
 
 ]
