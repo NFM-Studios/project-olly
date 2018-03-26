ebaa78eb117c258fbb89e57c849a7ae6decfb920
Unknown <ben@mulveyfamily.com>
2018-03-25 21:27:39 -0400

    Join tournaments
    no verification whatsoever

--- singletournaments/forms.py
+++ singletournaments/forms.py
@@ -3,6 +3,7 @@ from singletournaments.models import SingleEliminationTournament
 
 
 class SingleEliminationTournamentJoin(forms.ModelForm):
+    tournaments = forms.ModelChoiceField(queryset=SingleEliminationTournament.objects.all())
     class Meta:
         model = SingleEliminationTournament
         fields = ('teams', )
