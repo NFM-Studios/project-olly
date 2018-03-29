0928a8aef5decb1fb447d3465c9a08cc58aa9362
Mike M <mikemaddem@gmail.com>
2018-03-29 16:50:45 -0400

    fix pep 8, and tournament size8 rounds

--- singletournaments/views.py
+++ singletournaments/views.py
@@ -89,20 +89,28 @@ class SingleTournamentBracket(View):
 
         elif tournament.size == 8:
             # get 3 rounds to pass to the view
-
-            round1 = SingleTournamentRound.objects.get
             template_name = 'singletournaments/singletournament_bracket8.html'
+            round1 = SingleTournamentRound.objects.get(tournament=tournament, roundnum=1)
+            round2 = SingleTournamentRound.objects.get(tournament=tournament, roundnum=2)
+            round3 = SingleTournamentRound.objects.get(tournament=tournament, roundnum=3)
+
+
         elif tournament.size == 16:
             # get 4 rounds to pass to the view
             template_name = 'singletournaments/singletournament_bracket16.html'
+
+
         elif tournament.size == 32:
             # get 5 rounds to pass to the view
             template_name = 'singletournaments/singletournament_bracket32.html'
+
+
         elif tournament.size == 64:
             # get 6 rounds to pass to the view
             template_name = 'singletournaments/singletournament_bracket64.html'
+
+
         elif tournament.size == 128:
             # get 7 rounds to pass to the  view
             template_name = 'singletournaments/singletournament_bracket128.html'
-        return render(request, self.template_name,
-                      {'x': pk, 'tournament': tournament, 'round1': round1, 'round2': round2})
+            
