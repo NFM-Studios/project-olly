d0aaf4530e30e78904beda7658cdb59b6ef2cd25
Mike M <mikemaddem@gmail.com>
2018-04-02 16:32:21 -0400

    fix assigning teams

--- singletournaments/models.py
+++ singletournaments/models.py
@@ -140,24 +140,24 @@ class SingleEliminationTournament(models.Model):
         # create the round objects based on the tournament size
         if self.size == 4:
             # generate 2 rounds
-            round1 = SingleTournamentRound(matchesnum=2, roundnum=1, tournament=self)
+            round1 = SingleTournamentRound(matchesnum=2, roundnum=1, tournament=self, teams=self.teams)
             round1.save()
             round2 = SingleTournamentRound(matchesnum=1, roundnum=2, tournament=self)
             round2.save()
         elif self.size == 8:
             # generate 3 rounds
             round1 = SingleTournamentRound(matchesnum=4, roundnum=1, tournament=self, teams=self.teams)
-            round2 = SingleTournamentRound(matchesnum=2, roundnum=2, tournament=self, teams=self.teams)
-            round3 = SingleTournamentRound(matchesnum=1, roundnum=3, tournament=self, teams=self.teams)
+            round2 = SingleTournamentRound(matchesnum=2, roundnum=2, tournament=self)
+            round3 = SingleTournamentRound(matchesnum=1, roundnum=3, tournament=self)
             round1.save()
             round2.save()
             round3.save()
         elif self.size == 16:
             # generate 4 rounds
             round1 = SingleTournamentRound(matchesnum=8, roundnum=1, tournament=self, teams=self.teams)
-            round2 = SingleTournamentRound(matchesnum=4, roundnum=2, tournament=self, teams=self.teams)
-            round3 = SingleTournamentRound(matchesnum=2, roundnum=3, tournament=self, teams=self.teams)
-            round4 = SingleTournamentRound(matchesnum=1, roundnum=4, tournament=self, teams=self.teams)
+            round2 = SingleTournamentRound(matchesnum=4, roundnum=2, tournament=self)
+            round3 = SingleTournamentRound(matchesnum=2, roundnum=3, tournament=self)
+            round4 = SingleTournamentRound(matchesnum=1, roundnum=4, tournament=self)
             round1.save()
             round2.save()
             round3.save()
@@ -165,10 +165,10 @@ class SingleEliminationTournament(models.Model):
         elif self.size == 32:
             # generate 5 rounds
             round1 = SingleTournamentRound(matchesnum=16, roundnum=1, tournament=self, teams=self.teams)
-            round2 = SingleTournamentRound(matchesnum=8, roundnum=2, tournament=self, teams=self.teams)
-            round3 = SingleTournamentRound(matchesnum=4, roundnum=3, tournament=self, teams=self.teams)
-            round4 = SingleTournamentRound(matchesnum=2, roundnum=4, tournament=self, teams=self.teams)
-            round5 = SingleTournamentRound(matchesnum=1, roundnum=5, tournament=self, teams=self.teams)
+            round2 = SingleTournamentRound(matchesnum=8, roundnum=2, tournament=self)
+            round3 = SingleTournamentRound(matchesnum=4, roundnum=3, tournament=self)
+            round4 = SingleTournamentRound(matchesnum=2, roundnum=4, tournament=self)
+            round5 = SingleTournamentRound(matchesnum=1, roundnum=5, tournament=self)
             round1.save()
             round2.save()
             round3.save()
