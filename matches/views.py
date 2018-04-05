a398aa935bb8c5961b63e8e879d7209b8a6b7ffb
Unknown <ben@mulveyfamily.com>
2018-04-04 22:57:47 -0400

    Basic match report creation

--- matches/views.py
+++ matches/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from singletournaments.models import SingleEliminationTournament
-from teams.models import Team
+from django.contrib import messages
+from singletournaments.models import SingleEliminationTournament, SingleTournamentRound
+from teams.models import Team, TeamInvite
 from matches.models import Match
+from .forms import MatchReportCreateForm
 
 
 class TournamentMatchDetailView(DetailView):
@@ -11,7 +13,7 @@ class TournamentMatchDetailView(DetailView):
 
     def get(self, request, **kwargs):
         pk = self.kwargs['pk']
-        match = match.objects.get(id=pk)
+        match = Match.objects.get(id=pk)
         return render(request, self.template_name, {'x': pk, 'match': match})
 
 
@@ -21,9 +23,33 @@ class MatchReportCreateView(CreateView):
 
     def form_valid(self, form):
         report = form.instance
-        report.reporter = self.request.user
-        if report.reporter
-        report.save()
-        self.success_url = reverse('matches:detail', args=[matches.id])
-        messages.success(self.request, 'Your Report has been successfully submitted')
-        return super(MatchReportCreateView, self).form_valid(form)
+        report.reporting_user = self.request.user
+        match = Match.objects.get(id=form.data['match'])
+        team1 = Team.objects.get(id=match.hometeam_id)
+        team2 = Team.objects.get(id=match.awayteam_id)
+        team1_reporters = TeamInvite.objects.filter(team=team1, hasPerms=True)
+        team2_reporters = TeamInvite.objects.filter(team=team2, hasperms=True)
+        reporter_team = TeamInvite.objects.get(user=self.request.user, team=[team1, team2]).team
+        if report.reporter in team1_reporters or report.reporter in team2_reporters:
+            report.match = match
+            report.reporting_team = reporter_team.id
+            report.reported_winner = form.data['winner']
+            if reporter_team == team1:
+                match.team1reported = True
+                match.team1reportedwinner = report.reported_winner
+                match.team1reportedwinner_id = report.reported_winner.id
+            elif reporter_team == team2:
+                match.team2reported = True
+                match.team2reportedwinner = report.reported_winner
+                match.team2reportedwinner_id = report.reported_winner.id
+            else:
+                messages.error(self.request, "Something went wrong (this shouldn't be seen)")
+                return redirect('singletournaments:list')
+            match.save()
+            report.save()
+            self.success_url = reverse('matches:detail', args=[match.id])
+            messages.success(self.request, 'Your Report has been successfully submitted')
+            return super(MatchReportCreateView, self).form_valid(form)
+        else:
+            messages.error(self.request, "You don't have permissions to report on this match")
+            return redirect('singletournaments:list')
