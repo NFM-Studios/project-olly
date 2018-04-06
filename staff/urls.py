645babaf54df87db04fad1ba62c2995bd5b10587
Mike M <mikemaddem@gmail.com>
2018-04-05 20:50:39 -0500

    add , and remove as_view() for staff urls

--- staff/urls.py
+++ staff/urls.py
@@ -19,7 +19,8 @@ urlpatterns = [
     url(r'^users/edit/(?P<urlusername>\w+)/$', login_required(views.edituser)),
     url(r'^tournaments/$', login_required(views.tournaments), name='tournamentlist'),
     url(r'^tournaments/edit/(?P<pk>\d+)/$', login_required(views.edit_tournament), name='edit_tournament'),
-    url(r'^tournaments/create$', login_required(views.CreateTournament.as_view()), name='create_tournament')
-    url(r'^news/', login_required(views.news_index.as_view()), name='news_index')
-    url(r'^news/list', login_required(views.news_list.as_view()), name='news_list')
+    url(r'^tournaments/create$', login_required(views.CreateTournament.as_view()), name='create_tournament'),
+    url(r'^news/', login_required(views.news_index), name='news_index'),
+    url(r'^news/list', login_required(views.news_list), name='news_list'),
+    url(r'^news/create', login_required(views.create_article), name='create_article'),
 ]
