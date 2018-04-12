389d49e377b3993b6bfdd751bf7ac86a37481fa5
Unknown <ben@mulveyfamily.com>
2018-04-11 20:59:19 -0400

    Partial credit transfer&transaction lists in staff

--- profiles/forms.py
+++ profiles/forms.py
@@ -30,7 +30,7 @@ class EditProfileForm(forms.ModelForm):
         )
 
 
-class LeaderboardSortForm(forms.Form):      # it works but is messy af. should be replaced with something like http://img.mulveyben.me/img/chrome_2018-03-11_22-04-28.png
+class SortForm(forms.Form):      # it works but is messy af. should be replaced with something like http://img.mulveyben.me/img/chrome_2018-03-11_22-04-28.png
     sort_xp_asc = forms.BooleanField(required=False)
     sort_xp_desc = forms.BooleanField(required=False)
     sort_trophies_asc = forms.BooleanField(required=False)
