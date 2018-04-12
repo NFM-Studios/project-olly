7460755373cda7cae39c9b4f986731eb57be50ba
Mike M <mikemaddem@gmail.com>
2018-04-12 17:07:18 -0400

    remove teams from rounds

--- singletournaments/models.py
+++ singletournaments/models.py
@@ -248,7 +248,7 @@ class SingleEliminationTournament(models.Model):
 
 class SingleTournamentRound(models.Model):
     # ManyToManyField to keep track of the teams that are still active and have matches to play in the round
-    teams = models.ManyToManyField(Team)
+    # teams = models.ManyToManyField(Team)
 
     # what round number is this? round 1 is the first round of the tournament
     roundnum = models.PositiveSmallIntegerField(default=1)
