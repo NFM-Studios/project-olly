a93708ea07653dc5f1d78c854c797bb3935c29c5
Unknown <ben@mulveyfamily.com>
2018-04-15 11:29:07 -0400

    declare winner in the right place
    i think

--- matches/views.py
+++ matches/views.py
@@ -78,6 +78,31 @@ class MatchReportCreateView(CreateView):
                         match.disputed = True
                         match.save()
                         return redirect('matches:dispute', pk=form.data['match'])
+                    if match.team1reported:
+                        # team 1 reported
+                        if match.team1reportedwinner == team2:
+                            # team1 is reporting that team2 won
+                            # declare team2 as winner
+                            match.winner = team2
+                            match.save()
+                        elif match.team1reportedwinner == team1:
+                            # have to wait for the other team to confirm
+                            pass
+                    elif match.team2reported:
+                        if match.team2reportedwinner == team1:
+                            # team 1 wins
+                            match.winner = team1
+                            match.save()
+                        elif match.team2reportedwinner == team2:
+                            pass
+                    if match.team1reported and match.team2reported:
+                        if match.team2reportedwinner == team2 and match.team1reportedwinner == team2:
+
+                            match.winner = team2
+                            match.save()
+                        elif match.team2reportedwinner == team1 and match.team1reportedwinner == team1:
+                            match.winner = team1
+                            match.save()
                 self.success_url = reverse('matches:detail', args=[match.id])
                 messages.success(self.request, 'Your Report has been successfully submitted')
                 return redirect('matches:detail', pk=form.data['match'])
@@ -87,31 +112,6 @@ class MatchReportCreateView(CreateView):
         else:
             messages.error(request, "A report has already been created for this match")
             return redirect('matches:detail', pk=form.data['match'])
-        if match.team1reported:
-            # team 1 reported
-            if match.team1reportedwinner == team2:
-                # team1 is reporting that team2 won
-                # declare team2 as winner
-                match.winner = team2
-                match.save()
-            elif match.team1reportedwinner == team1:
-                # have to wait for the other team to confirm
-                pass
-        elif match.team2reported:
-            if match.team2reportedwinner == team1:
-                # team 1 wins
-                match.winner = team1
-                match.save()
-            elif match.team2reportedwinner == team2:
-                pass
-        if match.team1reported and match.team2reported:
-            if match.team2reportedwinner == team2 and match.team1reportedwinner == team2:
-
-                match.winner = team2
-                match.save()
-            elif match.team2reportedwinner == team1 and match.team1reportedwinner == team1:
-                match.winner = team1
-                match.save()
 
 
 
