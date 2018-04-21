f9030e7eec0e71455b74c6333ca7f584a6a70815
Steven <steven97@me.com>
2018-04-20 20:23:04 -0400

    Add version info to footer

--- olly/context_processors.py
+++ olly/context_processors.py
@@ -3,4 +3,5 @@ from django.conf import settings
 
 def site_info(request):
     return {'SITE_NAME': settings.SITE_NAME,
-    'SITE_SERVER': settings.SITE_SERVER}
+            'SITE_SERVER': settings.SITE_SERVER,
+            'SITE_VERSION': settings.SITE_VERSION}
