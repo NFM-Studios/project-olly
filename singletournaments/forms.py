88c0795bb02d4b37949f651706e0f390a6d9ab92
Unknown <ben@mulveyfamily.com>
2018-04-23 20:33:17 -0400

    Tournament join back on a separate page
    Join button on detail looks ugly tho

--- singletournaments/forms.py
+++ singletournaments/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from singletournaments.models import SingleEliminationTournament
-from teams.models import Team
+from teams.models import Team, TeamInvite
 
 
-class SingleEliminationTournamentJoin(forms.ModelForm):
+class SingleEliminationTournamentJoinGet(forms.ModelForm):
     teams = forms.ModelChoiceField(queryset=Team.objects.all())
     # tournaments = forms.ModelChoiceField(queryset=SingleEliminationTournament.objects.all())
 
@@ -11,8 +11,17 @@ class SingleEliminationTournamentJoin(forms.ModelForm):
         model = SingleEliminationTournament
         fields = ()
 
-    #def __init__(self, request, *args, **kwargs):
-    #    self.username = request.user
-    #    self.team = forms.ModelChoiceField(
-    #        queryset=TeamInvite.objects.filter(captain=['captain', 'founder'], user_id=self.username.id))
-    #    super(SingleEliminationTournamentJoin, self).__init__(*args, **kwargs)
+    def __init__(self, request, *args, **kwargs):
+        self.username = request.user
+        self.team = forms.ModelChoiceField(
+            queryset=TeamInvite.objects.filter(captain=['captain', 'founder'], user_id=self.username.id))
+        super(SingleEliminationTournamentJoinGet, self).__init__(*args, **kwargs)
+
+
+class SingleEliminationTournamentJoinPost(forms.ModelForm):
+    teams = forms.ModelChoiceField(queryset=Team.objects.all())
+    # tournaments = forms.ModelChoiceField(queryset=SingleEliminationTournament.objects.all())
+
+    class Meta:
+        model = SingleEliminationTournament
+        fields = ()
