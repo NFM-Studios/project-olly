4a7c5313dcc3755ea2ced79a7a4e36a1382f7d92
Steven <steven97@me.com>
2018-08-17 22:35:25 -0400

    Per site settings.py

--- olly/duel_wsgi.py
+++ olly/duel_wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "olly.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "olly.duel_settings")
 
 application = get_wsgi_application()
