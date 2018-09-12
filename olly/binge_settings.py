c6f8958aa046e2cb3c2ef0448f46309a3c530fe7
Steven <steven97@me.com>
2018-09-11 21:21:07 -0400

    Update settings and wsgi files

--- olly/binge_settings.py
+++ olly/binge_settings.py
@@ -2,7 +2,7 @@ from . base_settings import *
 import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bcgu@a)k$z!)1qmv@5a)&e$x@+@_tvl-s87)3@n)032*6r6u-2'
+SECRET_KEY = 'H8+;[TG8G$8nxGT([mT:wJY%KF]J:ur#C^-?*+/zvF^Z4)/PA>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -10,19 +10,19 @@ DEBUG = False
 # SECURITY WARNING: don't run with this set to true in prod
 PAYPAL_TEST = False
 
-ALLOWED_HOSTS = ['duel.nfmstudios.com','duelbattleroyale.com', 'duel.lan']
+ALLOWED_HOSTS = ['*']
 
-SITE_URL = 'duelbattleroyale.com'
+SITE_URL = 'binge.nfmstudios.com'
 
-WSGI_APPLICATION = 'olly.duel_wsgi:application'
+WSGI_APPLICATION = 'olly.binge_wsgi:application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'duel',
-        'USER': 'duel',
+        'NAME': 'binge',
+        'USER': 'binge',
         'PASSWORD': '9tAqopY6bVXnnoVnz7NCFojtQ3EIC2k5',
         'HOST': '10.136.73.223',
         'PORT': '',
@@ -32,10 +32,10 @@ DATABASES = {
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'olly/media')
 
-AWS_ACCESS_KEY_ID = 'ANARP7WZBRXNDXS6M6ES'
-AWS_SECRET_ACCESS_KEY = 'uy/4rMr2bPVqfKWm+3fSKZstNQqCWk3C1eAIW2b0WiM'
+AWS_ACCESS_KEY_ID = 'CSAMQ5GFQMTLT3FBSFWD'
+AWS_SECRET_ACCESS_KEY = 'Rkls5L2UG9/T9MfDGok0H912JHRjFSo1EnBJR6jdkRE'
 AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-AWS_STORAGE_BUCKET_NAME = 'duel'
+AWS_STORAGE_BUCKET_NAME = 'binge'
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = "%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_STORAGE_BUCKET_NAME)
@@ -43,9 +43,9 @@ MEDIA_URL = "%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_STORAGE_BUCKET_NAME)
 # Email stuff
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_HOST_USER = 'duel@mg.nfmstudios.com'
-EMAIL_HOST_PASSWORD = 'PBmks2LpvRbWrr5oPPtGpNyJmScJfDjf'
-FROM_EMAIL = "Duel Challenge <noreply@dueltournaments.com>"
+EMAIL_HOST_USER = 'binge@mg.nfmstudios.com'
+EMAIL_HOST_PASSWORD = 'U7THcY3Pam5vBaQWkNeasY6NYUCXgKTd'
+FROM_EMAIL = "Bingeplay <noreply@bingeplay.com>"
 EMAIL_PORT = 587
 PAYPAL_EMAIL = "omega.competitions@gmail.com"
 
@@ -54,5 +54,5 @@ GOOGLE_RECAPTCHA_SECRET_KEY = '6Lcj1GYUAAAAABo_n8XJrwvutsDRTHm6Iqv04rwK'
 GOOGLE_RECAPTCHA_SITE_KEY = '6Lcj1GYUAAAAAKzN1gxoYx-_Q6h0zagkwr9wpc_S'
 
 # Site info
-SITE_NAME = "DuelChallenge"
+SITE_NAME = "BingePlay"
 SITE_SERVER = "Wheezy"
--- olly/wlg_settings.py
+++ olly/wlg_settings.py
@@ -2,7 +2,7 @@ from . base_settings import *
 import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bcgu@a)k$z!)1qmv@5a)&e$x@+@_tvl-s87)3@n)032*6r6u-2'
+SECRET_KEY = '8k%8LQ2KBtbjVQfzDC7mN6mQUKJws9$reLCvGQxRK7Exr5pAik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -10,20 +10,20 @@ DEBUG = False
 # SECURITY WARNING: don't run with this set to true in prod
 PAYPAL_TEST = False
 
-ALLOWED_HOSTS = ['duel.nfmstudios.com','duelbattleroyale.com', 'duel.lan']
+ALLOWED_HOSTS = ['wlg.nfmstudios.com', 'worldleagueofgaming.com', 'wlg.lan']
 
-SITE_URL = 'duelbattleroyale.com'
+SITE_URL = 'wlg.nfmstudios.com'
 
-WSGI_APPLICATION = 'olly.duel_wsgi:application'
+WSGI_APPLICATION = 'olly.wlg_wsgi:application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'duel',
-        'USER': 'duel',
-        'PASSWORD': '9tAqopY6bVXnnoVnz7NCFojtQ3EIC2k5',
+        'NAME': 'wlg',
+        'USER': 'wlg',
+        'PASSWORD': '*9kWWhU5N@AgkcSUm4@A%!^YUk9fQR%o',
         'HOST': '10.136.73.223',
         'PORT': '',
     }
@@ -32,10 +32,10 @@ DATABASES = {
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'olly/media')
 
-AWS_ACCESS_KEY_ID = 'ANARP7WZBRXNDXS6M6ES'
-AWS_SECRET_ACCESS_KEY = 'uy/4rMr2bPVqfKWm+3fSKZstNQqCWk3C1eAIW2b0WiM'
+AAWS_ACCESS_KEY_ID = 'W74O67XJ3SJUTMCJNSBE'
+AWS_SECRET_ACCESS_KEY = 'mjNILBlckmn9cJT+11S+DofDIcZtDUxNPLoQQBtwdJk'
 AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-AWS_STORAGE_BUCKET_NAME = 'duel'
+AWS_STORAGE_BUCKET_NAME = 'wlg'
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = "%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_STORAGE_BUCKET_NAME)
@@ -43,16 +43,16 @@ MEDIA_URL = "%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_STORAGE_BUCKET_NAME)
 # Email stuff
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_HOST_USER = 'duel@mg.nfmstudios.com'
-EMAIL_HOST_PASSWORD = 'PBmks2LpvRbWrr5oPPtGpNyJmScJfDjf'
-FROM_EMAIL = "Duel Challenge <noreply@dueltournaments.com>"
+EMAIL_HOST_USER = 'wlg@mg.nfmstudios.com'
+EMAIL_HOST_PASSWORD = 'qBWbPcsf38VLGAW7CbHtPxgm7SrIcKBn'
+FROM_EMAIL = "World League of Gaming <noreply@worldleagueofgaming.com>"
 EMAIL_PORT = 587
-PAYPAL_EMAIL = "omega.competitions@gmail.com"
+PAYPAL_EMAIL = "loubravo@worldleaguegaming.com"
 
 # Captcha
-GOOGLE_RECAPTCHA_SECRET_KEY = '6Lcj1GYUAAAAABo_n8XJrwvutsDRTHm6Iqv04rwK'
-GOOGLE_RECAPTCHA_SITE_KEY = '6Lcj1GYUAAAAAKzN1gxoYx-_Q6h0zagkwr9wpc_S'
+GOOGLE_RECAPTCHA_SECRET_KEY = '6Lf9OFYUAAAAAF-24jSe5cxH8JPr_DCBMWhQJQuC'
+GOOGLE_RECAPTCHA_SITE_KEY = '6Lf9OFYUAAAAAJ-CIbE26ciHRwYIaPoVOkiB3pHj'
 
 # Site info
-SITE_NAME = "DuelChallenge"
+SITE_NAME = "WorldLeagueOfGaming"
 SITE_SERVER = "Wheezy"
