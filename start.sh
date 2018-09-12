74b798e97f035e456ac80c2b27871ce01b1433d2
Steven <steven97@me.com>
2018-09-11 21:21:52 -0400

    Olly dockerfile and start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo Current site is $site
+
+echo Running Migrations.
+python3 manage.py migrate --settings olly.$site"_settings"
+
+echo Collecting static files
+python3 manage.py collectstaic --noinput
+
+echo Starting Gunicorn.
+gunicorn olly.$site"_wsgi:application" \
+    --bind unix:/sock/$site".sock" \
+    --workers 3
