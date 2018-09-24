23ebcd95c0e4777af65f40885d8b46a25a6fc65d
Mike M <mikemaddem@gmail.com>
2018-09-24 13:24:14 -0400

    remove remaining taggit tag lines from news views

--- news/views.py
+++ news/views.py
@@ -9,13 +9,13 @@ from .forms import EmailPostForm, CommentForm
 app_name = 'news'
 
 
-def post_list(request, tag_slug=None): 
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
-    tag = None
+    #tag = None
     
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=[tag])
+    #if tag_slug:
+     #   tag = get_object_or_404(Tag, slug=tag_slug)
+      #  object_list = object_list.filter(tags__in=[tag])
     
     paginator = Paginator(object_list, 3)  # 3 post in each page.
     page = request.GET.get('page')
@@ -27,7 +27,7 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         # if the page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'news/' + request.tenant + '/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
+    return render(request, 'news/' + request.tenant + '/post/list.html', {'page': page, 'posts': posts})
 
 
 class PostListView(ListView): 
@@ -59,9 +59,9 @@ def post_detail(request, year, month, day, post):
         comment_form = CommentForm()
 
     # list of similar posts
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    #post_tags_ids = post.tags.values_list('id', flat=True)
+    #similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    #similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'news/' + request.tenant + 'post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'comment_form': comment_form,
