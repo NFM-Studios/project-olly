6d136fa7cfb0d51532c68575ffda165ce3b8bb9d
Mike M <mikemaddem@gmail.com>
2018-09-24 22:31:12 -0400

    ONE FKING / KEPT THE TEMPLATE FROM BREAKING - GOD DAMMIT

--- news/views.py
+++ news/views.py
@@ -62,7 +62,7 @@ def post_detail(request, year, month, day, post):
     #post_tags_ids = post.tags.values_list('id', flat=True)
     #similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
     #similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
-    return render(request, 'news/' + request.tenant + 'post/detail.html', {'post': post,
+    return render(request, 'news/' + request.tenant + '/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'comment_form': comment_form})
 
