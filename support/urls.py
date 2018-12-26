e0314604e725777a47245328df9ab4c3475f8b54
Steven <steven97@me.com>
2018-12-26 16:38:30 -0500

    First half of new URL routing syntax

--- support/urls.py
+++ support/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 from support.views import MyTicketListView, MyTicketDetailView, TicketCreateView
 
 app_name = 'support'
 
 urlpatterns = [
-    url(r'^$', login_required(MyTicketListView.as_view()), name='list'),
-    url(r'^my/(?P<pk>\d+)/$', login_required(MyTicketDetailView.as_view()), name='detail'),
-    url(r'^create/$', login_required(TicketCreateView.as_view()), name='create'),
+    path('', login_required(MyTicketListView.as_view()), name='list'),
+    path('my/<int:pk>/', login_required(MyTicketDetailView.as_view()), name='detail'),
+    path('create/', login_required(TicketCreateView.as_view()), name='create'),
 ]
