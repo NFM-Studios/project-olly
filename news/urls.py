e659b28b20043a9f87592553d270bb09afecb002
Steven Young <steven97@me.com>
2018-12-26 21:13:19 -0500

    More URL routing

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'news'
 
 urlpatterns = [
     # post views
-    url(r'^$', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
     url(r'^(?P<slug>[A-Za-z0-9_@+.-]+)/$', views.post_detail, name='post_detail'),
-    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
