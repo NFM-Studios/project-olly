bc36db2c87430e1d3913309c12745abf257ac96d
Steven Young <steven97@me.com>
2018-12-26 21:13:19 -0500

    More URL routing

--- singletournaments/urls.py
+++ singletournaments/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 from singletournaments import views as tournament_views
 
@@ -6,13 +6,13 @@ app_name = 'singletournaments'
 
 urlpatterns = [
 
-    url(r'^$', tournament_views.List.as_view(), name='list'),
-    url(r'^(?P<pk>\d+)/join/$', login_required(tournament_views.SingleTournamentJoin.as_view()), name='join'),
-    url(r'^(?P<pk>\d+)/$', tournament_views.SingleTournamentDetail.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/rules/$', login_required(tournament_views.SingleTournamentRules.as_view()), name='ruleset'),
-    url(r'^(?P<pk>\d+)/bracket/$', login_required(tournament_views.SingleTournamentBracket.as_view()), name='bracket'),
-    url(r'^(?P<pk>\d+)/teams/$', login_required(tournament_views.SingleTournamentTeamsList.as_view()), name='teams'),
-    url(r'^(?P<pk>\d+)/matches/$', login_required(tournament_views.SingleTournamentMatchList.as_view()), name='matches'),
-    url(r'^(?P<pk>\d+)/leave/$', login_required(tournament_views.SingleTournamentLeave.as_view()), name='leave'),
+    path('', tournament_views.List.as_view(), name='list'),
+    path('<int:pk>/join/', login_required(tournament_views.SingleTournamentJoin.as_view()), name='join'),
+    path('<int:pk>/', tournament_views.SingleTournamentDetail.as_view(), name='detail'),
+    path('<int:pk>/rules/', login_required(tournament_views.SingleTournamentRules.as_view()), name='ruleset'),
+    path('<int:pk>/bracket/', login_required(tournament_views.SingleTournamentBracket.as_view()), name='bracket'),
+    path('<int:pk>/teams/', login_required(tournament_views.SingleTournamentTeamsList.as_view()), name='teams'),
+    path('<int:pk>/matches/', login_required(tournament_views.SingleTournamentMatchList.as_view()), name='matches'),
+    path('<int:pk>/leave/', login_required(tournament_views.SingleTournamentLeave.as_view()), name='leave'),
 
 ]
