bb68f606735e2e32eb8627015b8a94f282986c48
Steven Young <steven97@me.com>
2018-12-26 21:13:19 -0500

    More URL routing

--- matches/urls.py
+++ matches/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 from matches.views import TournamentMatchDetailView, MatchReportCreateView, MatchDisputeReportCreateView, MatchList
 
@@ -7,10 +6,10 @@ app_name = 'matches'
 
 
 urlpatterns = [
-    url(r'^$', login_required(MatchList.as_view()), name='list'),
-    url(r'^(?P<pk>\d+)/$', login_required(TournamentMatchDetailView.as_view()), name='detail'),
-    url(r'^(?P<pk>\d+)/report/$', login_required(MatchReportCreateView.as_view()), name='report'),
-    url(r'^(?P<pk>\d+)/dispute/', login_required(MatchDisputeReportCreateView.as_view()), name='dispute'),
+    path('', login_required(MatchList.as_view()), name='list'),
+    path('<int:pk>/', login_required(TournamentMatchDetailView.as_view()), name='detail'),
+    path('<int:pk>/report/', login_required(MatchReportCreateView.as_view()), name='report'),
+    path('<int:pk>/dispute/', login_required(MatchDisputeReportCreateView.as_view()), name='dispute'),
 
 
 ]
--- olly/urls.py
+++ olly/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.urls import path
 from django.contrib import admin
 from profiles import views as profile_views
 from pages import views as pages_views
@@ -16,31 +17,31 @@ from paypal.standard.ipn import views as paypal_views
 handler404 = pages_views.notfound
 
 urlpatterns = [
-    url(r'^$', pages_views.index, name='index'),
-    url(r'^about/', pages_views.about, name='about'),
-    url(r'^terms/', pages_views.terms, name='terms'),
-    url(r'^partners/', pages_views.partners_page, name='partners'),
-    url(r'^privacy/', pages_views.privacy, name='privacy'),
-    url(r'^404/', pages_views.notfound),
-    url(r'^register/', profile_views.CreateUserFormView.as_view(), name='register'),
-    url(r'^login/', profile_views.login, {'template_name': 'profiles/login_form.html'}, name='login'),
-    url(r'^logout/', profile_views.logout, name='logout'),
-    url(r'^reset-password/$', profile_views.password_reset, name='reset_password'),
-    url(r'^reset-password/done/$', profile_views.password_reset_done, name='password_reset_done'),
+    path('', pages_views.index, name='index'),
+    path('about/', pages_views.about, name='about'),
+    path('terms/', pages_views.terms, name='terms'),
+    path('partners/', pages_views.partners_page, name='partners'),
+    path('privacy/', pages_views.privacy, name='privacy'),
+    path('404/', pages_views.notfound),
+    path('register/', profile_views.CreateUserFormView.as_view(), name='register'),
+    path('login/', profile_views.login, {'template_name': 'profiles/login_form.html'}, name='login'),
+    path('logout/', profile_views.logout, name='logout'),
+    path('reset-password/', profile_views.password_reset, name='reset_password'),
+    path('reset-password/done/', profile_views.password_reset_done, name='password_reset_done'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', profile_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset-password/complete$', profile_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^profile/', include('profiles.urls', namespace='profiles')),
-    url(r'^support/', include('support.urls',namespace='support')),
-    url(r'^teams/', include('teams.urls',namespace='teams')),
+    path('reset-password/complete/', profile_views.password_reset_complete, name='password_reset_complete'),
+    path('admin/', admin.site.urls),
+    path('profile/', include('profiles.urls', namespace='profiles')),
+    path('support/', include('support.urls',namespace='support')),
+    path('teams/', include('teams.urls',namespace='teams')),
     url(r"^activate/(?P<uidb64>[0-9A-Za-z_'\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
         profile_views.activate, name='activate'),
-    url(r'^news/', include('news.urls', namespace='news')),
-    url(r'^store/', include('store.urls', namespace='store')),
-    url(r'^paypal/', paypal_views.ipn, name="paypal-ipn"),
+    path('news/', include('news.urls', namespace='news')),
+    path('store/', include('store.urls', namespace='store')),
+    path('paypal/', paypal_views.ipn, name="paypal-ipn"),
     url(r'^(?i)staff/', include('staff.urls', namespace='staff')),
-    url(r'^tournaments/', include('singletournaments.urls', namespace='singletournaments')),
-    url(r'^matches/', include('matches.urls', namespace='matches'))
+    path('tournaments/', include('singletournaments.urls', namespace='singletournaments')),
+    path('matches/', include('matches.urls', namespace='matches'))
 ]
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
