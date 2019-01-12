5f96c5cb01b700b15c23001219c420d3810b1cb4
Mike M <mikemaddem@gmail.com>
2019-01-11 22:10:57 -0500

    2 team bye matches properly advance now!!!!

--- staff/views.py
+++ staff/views.py
@@ -261,9 +261,12 @@ def add_teams(request, pk):
             form = AddTournamentTeamForm(request.POST)
             if form.is_valid():
                 teamid = form.cleaned_data['teams']
-                tournament.teams.add(Team.objects.get(id=teamid))
-                tournament.save()
-                messages.success(request, 'Tournament has been updated')
+                try:
+                    tournament.teams.add(Team.objects.get(id=teamid))
+                    tournament.save()
+                    messages.success(request, 'The specified team has successfully been added to the tournament')
+                except:
+                    messages.error(request, 'An error has occured and the team was not added to the tournament')
                 return redirect('staff:tournament_detail', pk)
         else:
             tournament = SingleEliminationTournament.objects.get(pk=pk)
@@ -461,6 +464,26 @@ def ruleset_detail(request, pk):
             return render(request, 'staff/ruleset_detail.html', {'ruleset': ruleset})
 
 
+def mikes_super_function(pk, currentround, nextround):
+    tournament = SingleEliminationTournament.objects.get(pk=pk)
+    currentround = SingleTournamentRound.objects.get(pk=currentround)
+    nextround = SingleTournamentRound.objects.get(pk=nextround)
+
+    matches = currentround.matches.all()
+    bye2_count = 0
+
+    for i in matches:
+        if i.bye_2 is True and i.winner_id is None and i.hometeam_id is None and i.awayteam_id is None:
+            # there are 2 bye teams, there won't be a winner
+            bye2_count += 1
+        elif i.completed is False:
+            return False
+
+        # elif i.winner_id is None:
+         #   return False
+    return bye2_count
+
+
 def advance(request, pk):
     user = UserProfile.objects.get(user__username=request.user.username)
     allowed = ['superadmin', 'admin']
@@ -479,27 +502,51 @@ def advance(request, pk):
         matches = currentround.matches.all()
         for i in matches:
             if i.winner is None:
-                messages.error(request, "The current round is not complete")
-                return redirect('staff:tournamentlist')
+                if mikes_super_function(tournament.id, currentround.id, nextround.id) is False:
+                    messages.error(request, "Some matches in the current round do not have a winner set")
+                    return redirect('staff:tournamentlist')
+                else:
+                    mike = mikes_super_function(tournament.id, currentround.id, nextround.id)
+
+            #if i.completed is False:
+             #   messages.error(request, 'There is a match that is not yet marked as completed in the current round')
+              #  return redirect('staff:tournamentlist')
 
         winners = []
 
+
+
         for i in matches:
-            winners.append(i.winner)
-            team = Team.objects.get(id=i.winner_id)
-            team.num_matchwin += 1
+
             try:
-                team1 = Team.objects.get(id=i.loser_id)
-                team1.num_matchloss += 1
-                team1.save()
+                if i.winner is None:
+                    winners.append('BYE TEAM')
+
+                else:
+                    team1 = Team.objects.get(id=i.loser_id)
+                    team1.num_matchloss += 1
+                    team1.save()
+                    winners.append(i.winner)
+                    team = Team.objects.get(id=i.winner_id)
+                    team.num_matchwin += 1
+                    team.save()
             except:
                 pass
-            team.save()
+
+        # check to make sure mike +
+
 
         i = 0
         while i < len(winners):
-            newmatch = Match(game=tournament.game, platform=tournament.platform,
-                             awayteam=winners[i], hometeam=winners[i+1])
+            if winners[i] is 'BYE TEAM':
+                newmatch = Match(game=tournament.game, platform=tournament.platform, hometeam=winners[i+1])
+
+            elif winners[i+1] is 'BYE TEAM':
+                newmatch = Match(game=tournament.game, platform=tournament.platform,
+                                 awayteam=winners[i])
+            else:
+                newmatch = Match(game=tournament.game, platform=tournament.platform,
+                                 awayteam=winners[i], hometeam=winners[i+1])
             newmatch.save()
             nextround.matches.add(newmatch)
             i += 2
