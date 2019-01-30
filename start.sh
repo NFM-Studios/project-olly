ea30e89e0e18bd9eed17b475b85af15e44292b9e
Steven Young <steven97@me.com>
2019-01-29 20:52:06 -0500

    Automatically generate staticinfo if it doesn't exist

--- start.sh
+++ start.sh
@@ -10,6 +10,9 @@ python3 manage.py migrate --settings olly.$site"_settings"
 echo creating a superuser.
 python3 manage.py shell --settings olly.$site"_settings" -c "from django.contrib.auth.models import User; User.objects.create_superuser('NFMStudios', 'nfm.studios@gmail.com', 'Bigcatparadise!')"
 
+echo creating staticinfo
+python3 manage.py shell --settings olly.$site"_settings" -c "from pages.models import StaticInfo; StaticInfo.objects.get_or_create(pk=1);"
+
 echo Collecting static files
 python3 manage.py collectstatic --noinput
 
