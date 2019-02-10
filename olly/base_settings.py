0a9099c8ad8efd5e58591a5773b522527dcc8a36
Steven <steven97@me.com>
2019-02-10 16:32:27 -0500

    Ignore STATIC_ROOT as an unresolved reference

--- olly/base_settings.py
+++ olly/base_settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+# noinspection PyUnresolvedReferences
 STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
