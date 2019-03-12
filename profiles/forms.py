d863cb071c74ae4ff0de13b8fdcd66332b83f67f
Mike M <mikemaddem@gmail.com>
2019-03-12 19:51:50 -0400

    add email enabled styling, and remove force width

--- profiles/forms.py
+++ profiles/forms.py
@@ -46,27 +46,29 @@ class EditProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
         self.fields['profile_picture'].widget.attrs.update(
-            {'name': 'profile_picture', 'class': 'form-control', 'style': 'width:30%'})
-        self.fields['about_me'].widget.attrs.update({'name': 'email', 'class': 'form-control', 'style': 'width:30%'})
-        self.fields['xbl'].widget.attrs.update({'name': 'password', 'class': 'form-control', 'style': 'width:30%'})
-        self.fields['psn'].widget.attrs.update({'name': 'password_confirm', 'class': 'form-control',
-                                                'style': 'width:30%'})
-        self.fields['steam'].widget.attrs.update({'name': 'password_confirm', 'class': 'form-control', 'style':
-                                                  'width:30%'})
-        self.fields['epic'].widget.attrs.update({'name': 'password_confirm', 'class': 'form-control', 'style':
-                                                 'width:30%'})
-        self.fields['battlenet'].widget.attrs.update({'name': 'password_confirm', 'class': 'form-control', 'style':
-                                                      'width:30%'})
-        self.fields['twitter_profile'].widget.attrs.update({'name': 'password', 'class': 'form-control', 'style':
-                                                            'width:30%'})
-        self.fields['twitch_channel'].widget.attrs.update({'name': 'password', 'class': 'form-control', 'style':
-                                                           'width:30%'})
-        self.fields['favorite_game'].widget.attrs.update({'name': 'password', 'class': 'form-control', 'style':
-                                                          'width:30%'})
-        self.fields['favorite_console'].widget.attrs.update({'name': 'password', 'class': 'form-control', 'style':
-                                                             'width:30%'})
-        self.fields['country'].widget.attrs.update({'name': 'password', 'class': 'form-control',
-                                                    'style': 'background-color: black;width:30%'})
+            {'name': 'profile_picture', 'class': 'form-control', 'style': ''})
+        self.fields['about_me'].widget.attrs.update({'name': 'about_me', 'class': 'form-control', 'style': ''})
+        self.fields['xbl'].widget.attrs.update({'name': 'xbl', 'class': 'form-control', 'style': ''})
+        self.fields['psn'].widget.attrs.update({'name': 'psn', 'class': 'form-control',
+                                                'style': ''})
+        self.fields['steam'].widget.attrs.update({'name': 'steam', 'class': 'form-control', 'style':
+                                                  ''})
+        self.fields['epic'].widget.attrs.update({'name': 'epic', 'class': 'form-control', 'style':
+                                                 ''})
+        self.fields['battlenet'].widget.attrs.update({'name': 'battlenet', 'class': 'form-control', 'style':
+                                                      ''})
+        self.fields['twitter_profile'].widget.attrs.update({'name': 'twitter_profile', 'class': 'form-control', 'style':
+                                                            ''})
+        self.fields['twitch_channel'].widget.attrs.update({'name': 'twitch_channel', 'class': 'form-control', 'style':
+                                                           ''})
+        self.fields['favorite_game'].widget.attrs.update({'name': 'favorite_game', 'class': 'form-control', 'style':
+                                                          ''})
+        self.fields['favorite_console'].widget.attrs.update({'name': 'favorite_console', 'class': 'form-control', 'style':
+                                                             ''})
+        self.fields['country'].widget.attrs.update({'name': 'country', 'class': 'form-control',
+                                                    'style': 'background-color: black;'})
+        self.fields['email_enabled'].widget.attrs.update({'name': 'email_enabled', 'class': 'form-control',
+                                                    'style': 'background-color: black;'})
 
 
 class SortForm(forms.Form):  # it works but is messy af. should be replaced with something like
