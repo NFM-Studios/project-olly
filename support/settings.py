a1de04e7a36145a94470ce21601eb6d3a001292c
Mike M <mikemaddem@gmail.com>
2019-03-28 19:24:45 -0400

    remove support.category_choices - we use objects now instead

--- support/settings.py
+++ support/settings.py
@@ -9,15 +9,5 @@ STATUS_CHOICES = getattr(settings, 'TICKETS_STATUS_CHOICES', (
     (3, 'Closed'),
 ))
 
-CATEGORY_CHOICES = getattr(settings, 'TICKETS_CATEGORY_CHOICES', (
-    (0, 'General'),
-    (1, 'Prize Claim'),
-    (2, 'Tournament Support'),
-    (3, 'Billing'),
-    (4, 'Refund Request'),
-    (5, 'PSN/XBL Issues'),
-    (6, 'Match Support/Dispute'),
-))
-
 # List of the different status that define a ticket as closed.
 CLOSED_STATUSES = getattr(settings, 'TICKETS_CLOSED_STATUSES', (3, 4))
