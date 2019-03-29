bd5601af2df78051ea4a7a5b4d66e8fad1e36038
Steven <steven97@me.com>
2019-03-28 21:12:07 -0400

    Finish object based maps and pools

--- staff/views.py
+++ staff/views.py
@@ -943,7 +943,7 @@ def delete_map(request, pk):
         mapchoice = MapChoice.objects.get(pk=pk)
         mapchoice.delete()
         messages.success(request, "Map Deleted")
-        return redirect('staff:platformlist')
+        return redirect('staff:map_list')
 
 
 def create_mapchoice(request):
@@ -961,7 +961,7 @@ def create_mapchoice(request):
                 mapchoice = form.instance
                 mapchoice.save()
                 messages.success(request, 'Created Map')
-                return redirect('staff:maplist')
+                return redirect('staff:map_list')
             else:
                 form = GameChoiceForm(request.POST, request.FILES)
                 return render(request, 'staff/editmap.html', {'form': form})
@@ -993,9 +993,30 @@ def map_pool_detail(request, pk):
             else:
                 return render(request, 'staff/editmappool.html', {'form': form})
         else:
-            mapchoice = MapChoice.objects.get(pk=pk)
-            form = MapChoiceForm(instance=mapchoice)
-            return render(request, 'staff/editmap.html', {'form': form, 'pk': pk})
+            mappoolchoice = MapPoolChoice.objects.get(pk=pk)
+            maps = mappoolchoice.maps.all()
+            form = MapPoolChoiceForm(instance=mappoolchoice)
+            return render(request, 'staff/editmappool.html', {'form': form, 'maps':maps, 'pk': pk})
+
+
+def add_map_to_pool(request, pk):
+    user = UserProfile.objects.get(user__username=request.user.username)
+    allowed = ['superadmin', 'admin']
+    if user.user_type not in allowed:
+        return render(request, 'staff/permissiondenied.html')
+    else:
+        mappool = MapPoolChoice.objects.get(pk=pk)
+        maps = MapChoice.objects.filter(game=mappool.game)
+        if request.method == 'POST':
+            form = AddMapForm(maps, request.POST)
+            mapobj = form.data['mapobj']
+            mappool.add_map(mapobj)
+            messages.success(request, 'Map has been added to the pool')
+            return redirect('staff:map_pool_detail', pk=pk)
+
+        else:
+            form = AddMapForm(maps)
+            return render(request, 'staff/add_map.html', {'form': form, 'pk': pk})
 
 
 def delete_map_pool(request, pk):
@@ -1017,10 +1038,10 @@ def create_map_pool_choice(request):
         return render(request, 'staff/permissiondenied.html')
     else:
         if request.method == 'GET':
-            form = MapChoiceForm()
+            form = MapPoolChoiceForm()
             return render(request, 'staff/editmappool.html', {'form': form})
         else:
-            form = MapChoiceForm(request.POST, request.FILES)
+            form = MapPoolChoiceForm(request.POST, request.FILES)
             if form.is_valid():
                 mappoolchoice = form.instance
                 mappoolchoice.save()
