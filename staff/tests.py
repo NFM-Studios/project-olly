460269fb7529ea92da09b463906544d39c7c5464
Mike M <mikemaddem@gmail.com>
2019-03-28 20:15:13 -0400

    setup and test staff index response

--- staff/tests.py
+++ staff/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory
+from profiles.models import *
+from .views import *
 
-# Create your tests here.
+class StaffBasicTest1(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='testuser', email='test@nfmstudios.com', password='password')
+        profile = UserProfile.objects.get(user__username=self.user.username)
+        profile.user_type = 'superadmin'
+        profile.save()
+
+    def test_indexresponse(self):
+        request = self.factory.get('/staff')
+        request.tenant = 'eot'
+        request.user = self.user
+        response = staffindex(request)
+        self.assertEqual(response.status_code, 200)
