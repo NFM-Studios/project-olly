3bf7271eeee3bdfa9a5bcd6320260e09a8881235
Steven <steven97@me.com>
2019-03-31 16:16:46 -0400

    StaticInfo object gets passed with every request

--- olly/context_processors.py
+++ olly/context_processors.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from pages.models import StaticInfo
 
 
 def site_info(request):
     return {'SITE_NAME': settings.SITE_NAME,
             'SITE_SERVER': settings.SITE_SERVER,
-            'SITE_VERSION': settings.SITE_VERSION}
+            'SITE_VERSION': settings.SITE_VERSION,
+            'staticinfo': StaticInfo.objects.get(pk=1)}
