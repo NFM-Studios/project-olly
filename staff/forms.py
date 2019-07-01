cce797bf7fb6a6c0cb0d37e5084930edab27b329
Mike M <mm8476@rit.edu>
2019-07-01 12:59:17 -0400

    trying to fix form widgets for map pool

--- staff/forms.py
+++ staff/forms.py
@@ -264,7 +264,7 @@ class AddMapForm(forms.Form):
         super(AddMapForm, self).__init__(*args, **kwargs)
 
         self.fields['mapobj'] = forms.TypedMultipleChoiceField(choices=tuple([(i.pk, i.name) for i in maps]),
-                                                          widget=forms.CheckboxSelectMultiple())
+                                                               widget=forms.CheckboxSelectMultiple(), coerce=MapChoice)
 
     class Meta:
         fields = ('mapobj',)
