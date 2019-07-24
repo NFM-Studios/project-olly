d5ed33d4b5ec6d632b61717bab8074e0c53604a2
Mike M <mikemaddem@gmail.com>
2019-07-24 11:34:00 -0400

    map pool detail url

--- matches/urls.py
+++ matches/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from matches.views import TournamentMatchDetailView, MatchReportCreateView, MatchDisputeReportCreateView, MatchList
+from matches.views import *
 
 app_name = 'matches'
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<int:pk>/', login_required(TournamentMatchDetailView.as_view()), name='detail'),
     path('<int:pk>/report/', login_required(MatchReportCreateView.as_view()), name='report'),
     path('<int:pk>/dispute/', login_required(MatchDisputeReportCreateView.as_view()), name='dispute'),
+    path('maps/<int:pk>/', login_required(MapPoolDetail.as_view()), name='maps_detail')
 
 ]
