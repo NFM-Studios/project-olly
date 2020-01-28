ac93d6c2d42a6cd42cdf4fee7f18fb346cd754cd
Steven Young <steven97@me.com>
2020-01-28 18:08:55 -0500

    Remove unused js file

--- project-static/js/nfm.js
+++
@@ -1,3 +0,0 @@
-console.log("Hey bud, watchu doing looking here?");
-console.log("We don't use much JS as you can tell, rip");
-console.log("You should really follow NFM Studios on Twitter @NFM_Studios :D");
