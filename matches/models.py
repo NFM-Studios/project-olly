8c2bdc8469ac76cdf040eac539413c566cb8140a
Steven Young <steven97@me.com>
2020-02-16 13:08:54 -0500

    Remove null=True from bestof

--- matches/models.py
+++ matches/models.py
@@ -91,7 +91,7 @@ class Match(models.Model):
     winner = models.ForeignKey(Team, related_name='champions', on_delete=models.SET_NULL, null=True)
     loser = models.ForeignKey(Team, related_name='loser', on_delete=models.SET_NULL, null=True)
     # set the default map format to best of 1
-    bestof = models.SmallIntegerField(choices=MAPFORMAT_CHOICES, default=0, null=True)
+    bestof = models.SmallIntegerField(choices=MAPFORMAT_CHOICES, default=0)
     #          by default set it to be a 2v2.
     teamformat = models.SmallIntegerField(choices=TEAMFORMAT_CHOICES, default=1)
 
