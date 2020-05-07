238cf9d2ff4a2e5cf23b2da719a384743ca87bea
Steven Young <steven@nightfury.pw>
2020-05-07 18:13:49 -0400

    Allow pages to load if a SocialInfo object doesn't exist

--- olly/context_processors.py
+++ olly/context_processors.py
@@ -1,10 +1,17 @@
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+
 from pages import models as pagesmodels
+
+try:
+    SocialInfo = pagesmodels.SocialInfo.objects.get(pk=1)
+except ObjectDoesNotExist:
+    SocialInfo = None
 
 
 def site_info(request):
     return {'SITE_NAME': settings.SITE_NAME,
             'SITE_SERVER': settings.SITE_SERVER,
             'SITE_VERSION': settings.SITE_VERSION,
-			'SocialInfo': pagesmodels.SocialInfo.objects.get(pk=1),
+            'SocialInfo': SocialInfo,
             'ESPORTS_MODE': settings.ESPORTS_MODE}
