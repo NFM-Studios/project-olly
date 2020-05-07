5b8722ecd2cad321eedfb4a724e398fa9d7ddd8d
Michael Madden <mikemaddem@gmail.com>
2020-05-07 14:33:30 -0400

    rename templates to line up with app name

--- support/views.py
+++ support/views.py
@@ -16,7 +16,7 @@ class MyTicketListView(View):
     def get(self, request):
         form = self.form
         ticket_list = Ticket.objects.filter(creator=request.user, status__lte=2)
-        return render(request, 'tickets/ticket_list.html',
+        return render(request, 'support/ticket_list.html',
                       {'form': form, 'ticket_list': ticket_list})
 
     def post(self, request):
@@ -30,7 +30,7 @@ class MyTicketListView(View):
                 ticket_list = Ticket.objects.filter(pk=query)
             except ValueError:
                 ticket_list = Ticket.objects.filter(text__contains=query)
-        return render(request, 'tickets/ticket_list.html',
+        return render(request, 'support/ticket_list.html',
                       {'form': form, 'ticket_list': ticket_list})
 
 
@@ -49,7 +49,7 @@ class MyTicketDetailView(DetailView):
         ticket = get_object_or_404(Ticket, pk=pk)
         creator = UserProfile.objects.get(user=ticket.creator)
         comments = TicketComment.objects.filter(ticket=pk)
-        return render(request, 'tickets/ticket_detail.html', {'form': form1, 'x': pk, "ticket": ticket,
+        return render(request, 'support/ticket_detail.html', {'form': form1, 'x': pk, "ticket": ticket,
                                                               "comments": comments,
                                                               'creator': creator})
 
@@ -93,7 +93,7 @@ class TicketCreateView(View):
 
     def get(self, request):
         form = self.form_class()
-        return render(request, 'tickets/ticket_create.html', {'form': form})
+        return render(request, 'support/ticket_create.html', {'form': form})
 
     def post(self, request):
         form = self.form_class(request.POST)
