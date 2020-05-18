d2a3b33ce388123a3c1fac7b9d857a10d91de343
Michael Madden <mikemaddem@gmail.com>
2020-05-18 13:34:51 -0400

    stub out basic views for front end leagues

--- leagues/views.py
+++ leagues/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import League, LeagueDivision, LeagueSettings
 
-# Create your views here.
+def list_leagues(request):
+    pass
+
+
+def detail_league(request, pk):
+    pass
+
+
+def join_league(request, pk):
+    pass
+
+
+def detail_league_teams(request, pk):
+    pass
+
+
+def detail_league_divisions(request, pk):
+    pass
+
+
+def detail_league_rules(request, pk):
+    pass
