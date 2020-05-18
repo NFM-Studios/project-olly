31de738e49b6d023ec5b071c995dd6e5b811b125
Michael Madden <mikemaddem@gmail.com>
2020-05-18 13:58:58 -0400

    list league and detail league from end view

--- leagues/views.py
+++ leagues/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import League, LeagueDivision, LeagueSettings
 
+
 def list_leagues(request):
-    pass
+    leagues = League.objects.filter(active=True)
+    return render(request, 'leagues/leagues_list.html', {'leagues': leagues})
 
 
 def detail_league(request, pk):
-    pass
+    league = get_object_or_404(League, pk)
+    return render(request, 'leagues/league_detail.html', {'league': league})
 
 
 def join_league(request, pk):
