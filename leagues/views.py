aa66edbef234745cc6479623a832a15ae19405e3
Michael Madden <mikemaddem@gmail.com>
2020-11-13 15:42:36 -0500

    league detail show matches if only 1 division
    stub out join league view function

--- leagues/views.py
+++ leagues/views.py
@@ -10,16 +10,33 @@ def list_leagues(request):
 
 
 def detail_league(request, pk):
-    league = get_object_or_404(League)
+    league = get_object_or_404(League, pk=pk)
     teams = league.teams.all()
+    if len(league.divisions) == 1:
+        # there is only one division show the matches in the one page
+        division = league.divisions.first()
+        matches = division.matches
+        return render(request, 'leagues/league_division.html',
+                      {'league': league, 'matches': matches, 'division': division})
     return render(request, 'leagues/league_detail.html', {'league': league, 'teams': teams})
 
 
 def join_league(request, pk):
-    pass
+    league = get_object_or_404(League, pk=pk)
+    # TODO - create join league form and send to template
+    if request.method == 'GET':
+        # send the form
+        pass
+    elif request.method == 'POST':
+        # try and get them to join the league
+        # make sure enough players exist on the team
+        pass
+
+    return render(request, 'leagues/league_join.html', {'league': league})
 
 
 def leave_league(request, pk):
+    # find out which team wants to leave
     pass
 
 
@@ -28,7 +45,7 @@ def detail_league_teams(request, pk):
 
 
 def list_league_divisions(request, pk):
-    league = get_object_or_404(League)
+    league = get_object_or_404(League, pk=pk)
     if league.divisions.count() == 0:
         messages.warning(request, "There are no divisions for this league yet")
         return redirect('league:detail', pk)
@@ -39,7 +56,7 @@ def list_league_divisions(request, pk):
 
 
 def detail_league_division(request, pk, divid):
-    league = get_object_or_404(League)
+    league = get_object_or_404(League, pk=pk)
     division = LeagueDivision.objects.get(pk=divid)
     matches = division.matches.all()
     return render(request, 'leagues/league_division.html', {'league': league, 'division': division, 'matches': matches})
