e97f93d1099e80015e4640ed64d8bb83a3731a24
Michael Madden <mikemaddem@gmail.com>
2021-01-08 14:37:47 -0500

    register other match objects to admin panel

--- matches/admin.py
+++ matches/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Match
+from .models import Match, MapChoice, MapPoolChoice, MatchCheckIn
 
 admin.site.register(Match)
+admin.site.register(MapChoice)
+admin.site.register(MapPoolChoice)
+admin.site.register(MatchCheckIn)
