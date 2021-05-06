8f2f6c2e3c70d6b944add75fc9fd65d6d0343ba5
Mike M <mikemaddem@users.noreply.github.com>
2021-05-06 15:10:54 -0400

    Update forms.py

--- profiles/forms.py
+++ profiles/forms.py
@@ -43,13 +43,9 @@ class EditProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
         self.fields['feat_team'].queryset = self.instance.founder_teams.all() | self.instance.captain_teams.all() | self.instance.player_teams.all()
-        # super(EditProfileForm, self).__init__(*args, **kwargs)
-        # self.fields['email_enabled'].widget.attrs.update({'name': 'email_enabled', 'class': 'form-control',
-        #                                                  'style': 'background-color: black;'})
 
 
-class SortForm(forms.Form):  # it works but is messy af. should be replaced with something like
-    # http://img.mulveyben.me/img/chrome_2018-03-11_22-04-28.png
+class SortForm(forms.Form):  # to be improved
     sort_xp_asc = forms.BooleanField(required=False)
     sort_xp_desc = forms.BooleanField(required=False)
     sort_trophies_asc = forms.BooleanField(required=False)
