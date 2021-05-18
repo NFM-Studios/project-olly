0349e3ae54bccaf430f59aa1a9a7db43f489a327
Michael Madden <mikemaddem@gmail.com>
2021-05-18 16:30:29 -0400

    staff banrecord and unban record models

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class UserBanRecord(models.Model):
+    profile = models.ForeignKey('profiles.UserProfile', related_name='banned_profile', on_delete=models.SET_NULL, null=True)
+    reason = models.TextField(default="Not specified")
+    admin = models.ForeignKey('profiles.UserProfile', related_name='ban_admin', on_delete=models.SET_NULL, null=True)
+    datetime = models.DateTimeField(auto_created=True)
+
+
+class UserUnbanRecord(models.Model):
+    profile = models.ForeignKey('profiles.UserProfile', related_name='unbanned_profile', on_delete=models.SET_NULL,
+                                null=True)
+    reason = models.TextField(default="Not specified")
+    admin = models.ForeignKey('profiles.UserProfile', related_name='unban_admin', on_delete=models.SET_NULL, null=True)
+    datetime = models.DateTimeField(auto_created=True)
